53ea114e1d247a1062b62f5e68d4d306127a44ef
otmane1672-bit <otmane1672bit@users.noreply.github.com>
2026-02-12 08:06:00 +0200

    Fix: Remove custom WebViewClient to fix localhost error
    - Reverted MainActivity to use Capacitor default WebViewClient
    - This fixes 'ERR_CONNECTION_REFUSED' on startup
    - Status bar transparency is preserved in setupStatusBar()
    - UI padding is handled by www/index.html CSS

--- android/app/src/main/java/com/darkai/app/MainActivity.java
+++ android/app/src/main/java/com/darkai/app/MainActivity.java
@@ -1,9 +1,6 @@
 package com.darkai.app;
 
 import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.graphics.Color;
 import com.getcapacitor.BridgeActivity;
 
@@ -14,42 +11,11 @@ public class MainActivity extends BridgeActivity {
         super.onCreate(savedInstanceState);
         
         setupStatusBar();
-        setupWebView();
     }
     
     private void setupStatusBar() {
         // Transparent status bar
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         getWindow().setNavigationBarColor(Color.parseColor("#0b0c0f"));
-    }
-    
-    private void setupWebView() {
-        WebView webView = getBridge().getWebView();
-        
-        // Inject CSS to fix status bar overlap
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                
-                // Add padding for status bar
-                int statusBarHeight = getStatusBarHeight();
-                view.evaluateJavascript(
-                    "javascript:(function() {" +
-                    "var style = document.createElement('style');" +
-                    "style.innerHTML = 'body { padding-top: " + statusBarHeight + "px !important; }';" +
-                    "document.head.appendChild(style);" +
-                    "})();", null
-                );
-            }
-        });
-    }
-    
-    private int getStatusBarHeight() {
-        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            return getResources().getDimensionPixelSize(resourceId);
-        }
-        return 0;
     }
 }
