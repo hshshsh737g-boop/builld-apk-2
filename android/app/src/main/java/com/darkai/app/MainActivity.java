c04639d69282768c53ca99e7f6373d3e7280a934
otmane1672-bit <otmane1672bit@users.noreply.github.com>
2026-02-14 15:34:42 +0200

    Add WebView performance optimizations and cookie persistence

--- android/app/src/main/java/com/darkai/app/MainActivity.java
+++ android/app/src/main/java/com/darkai/app/MainActivity.java
@@ -2,6 +2,10 @@ package com.darkai.app;
 
 import android.os.Bundle;
 import android.graphics.Color;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.CookieManager;
+import android.view.View;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -11,11 +15,45 @@ public class MainActivity extends BridgeActivity {
         super.onCreate(savedInstanceState);
         
         setupStatusBar();
+        setupWebView();
     }
     
     private void setupStatusBar() {
         // Transparent status bar
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         getWindow().setNavigationBarColor(Color.parseColor("#0b0c0f"));
+    }
+    
+    private void setupWebView() {
+        WebView webView = getBridge().getWebView();
+        WebSettings settings = webView.getSettings();
+        
+        // Enable storage APIs
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        
+        // Enable cookies with third-party support
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+        cookieManager.setAcceptThirdPartyCookies(webView, true);
+        
+        // Cache settings for better performance
+        settings.setAppCacheEnabled(true);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        
+        // Hardware acceleration for smoother rendering
+        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        
+        // Additional performance tweaks
+        settings.setLoadsImagesAutomatically(true);
+        settings.setJavaScriptEnabled(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Flush cookies to disk when app goes to background
+        CookieManager.getInstance().flush();
     }
 }
